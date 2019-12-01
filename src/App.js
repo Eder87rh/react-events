65d50908c755b84c889ab2ae0833e47a3a565662
Eder Ramírez Hernández <eder.ramirez87@gmail.com>
2019-12-01 17:52:36 -0600

    Creating the Context

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 import Header from './components/Header';
+
+import CategoriasProvider from './context/CategoriasContext';
 
 
 function App() {
   return (
-    <Fragment>
-      <h1>hola</h1>
+    <CategoriasProvider>
       <Header/>
-    </Fragment>
+    </CategoriasProvider>
   );
 }
 
