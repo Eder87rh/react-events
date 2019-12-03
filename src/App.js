506cc8f0553a1d158736832b45ef47ab575cc160
Eder Ramirez <eder.ramirez87@gmail.com>
2019-12-03 08:32:15 -0600

    Creating the form

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import Header from './components/Header';
 
 import CategoriasProvider from './context/CategoriasContext';
+import Formulario from './components/Formulario';
 
 
 function App() {
   return (
     <CategoriasProvider>
       <Header/>
+      <div className="uk-container">
+      <Formulario/>
+      </div>
     </CategoriasProvider>
   );
 }
