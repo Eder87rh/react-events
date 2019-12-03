fa5fab4edba27ec74d2923294d46101eac08f3d6
Eder Ramirez <eder.ramirez87@gmail.com>
2019-12-03 09:23:24 -0600

    making event search with Context API

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import Header from './components/Header';
+import Formulario from './components/Formulario';
 
 import CategoriasProvider from './context/CategoriasContext';
-import Formulario from './components/Formulario';
+import EventosProvider from './context/EventosContext';
 
 
 function App() {
   return (
-    <CategoriasProvider>
-      <Header/>
-      <div className="uk-container">
-      <Formulario/>
-      </div>
-    </CategoriasProvider>
+    <EventosProvider>
+      <CategoriasProvider>
+        <Header/>
+        <div className="uk-container">
+          <Formulario/>
+        </div>
+      </CategoriasProvider>
+    </EventosProvider>
   );
 }
 
--- src/context/EventosContext.js
+++ src/context/EventosContext.js
@@ -1,21 +1,41 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 const EventosContext = React.createContext();
 export const EventosConsumer = EventosContext.Consumer;
 
 class EventosProvider extends Component {
   token = 'XHBAGJK6YXZZGK6MS4J3';
-  ordernar = 'date'
+  ordenar = 'date';
 
-  state = {  }
+  state = { 
+    eventos: []
+  }
 
   obtenerEventos = async (busqueda) => {
     let url = `https://www.eventbriteapi.com/v3/events/search/`+
     `?q=${busqueda.nombre}&categories=${busqueda.categoria}&sort_by=${this.ordenar}&token=${this.token}&locale=es_ES`;
+
+    // Consultar la API con la URL
+    const eventos = await axios(url);
+    console.log("TCL: EventosProvider -> obtenerEventos -> eventos", eventos.data.events)
+
+    this.setState({
+      eventos: eventos.data.events
+    })
   }
 
   render() { 
-    return (  );
+    return (
+      <EventosContext.Provider 
+        value={{
+          eventos: this.state.eventos,
+          obtenerEventos: this.obtenerEventos
+        }}
+      >
+        {this.props.children}
+      </EventosContext.Provider>
+    );
   }
 }
  
